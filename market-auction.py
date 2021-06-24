0e3704fb97d952ac5f8d3f1b2b38c2ca59fb90d7
Adrian Escalante <adresc@gmail.com>
2021-06-24 16:32:28 -0500

    adding functionality to make it work for years with no data

--- market-auction.py
+++ market-auction.py
@@ -1,23 +1,51 @@
 import json
+import sys
 
 INPUT_JSON_FILENAME = 'api-response.json'
 
 def calc_market_auction(model, year, data):
+    model_data = None
+    cost = None
+
     try:
+        model_data = data[model]
         cost = data[model]['saleDetails']['cost']
-        market_ratio  = data[model]['schedule']['years'][year]['marketRatio']
-        auction_ratio = data[model]['schedule']['years'][year]['auctionRatio']
-        # print(cost, market_ratio, auction_ratio)
+    except KeyError:
+        print("Error: Wrong Model ID")
+        return
+
+    try:
+        market_ratio  = model_data['schedule']['years'][year]['marketRatio']
+        auction_ratio = model_data['schedule']['years'][year]['auctionRatio']
         return {
             'Market':  cost * market_ratio,
             'Auction': cost * auction_ratio
         }
     except KeyError:
-        print("Error: Wrong Model ID or no Year data")
-        return
-    except:
-        print("Error: calculating MarketValue and AuctionValue")
-        return
+        # Calculate Year with no data
+        first_year = list(model_data['schedule']['years'].keys())[0]
+        last_year = list(model_data['schedule']['years'].keys())[-1]
+        default_market_ratio = model_data['schedule']['defaultMarketRatio']
+        default_auction_ratio = model_data['schedule']['defaultAuctionRatio']
+
+        if year > last_year:
+            years_diff = int(year) - int(last_year)
+            market_ratio  = float(model_data['schedule']['years'][last_year]['marketRatio'])
+            auction_ratio = float(model_data['schedule']['years'][last_year]['auctionRatio'])
+            calc_market_ratio  = (default_market_ratio  + 1) ** years_diff * market_ratio
+            calc_auction_ratio = (default_auction_ratio + 1) ** years_diff * auction_ratio
+        else:
+            years_diff = int(first_year) - int(year)
+            market_ratio  = float(model_data['schedule']['years'][first_year]['marketRatio'])
+            auction_ratio = float(model_data['schedule']['years'][first_year]['auctionRatio'])
+            calc_market_ratio  = (1 - default_market_ratio ) ** years_diff * market_ratio
+            calc_auction_ratio = (1 - default_auction_ratio) ** years_diff * auction_ratio
+        
+        return {
+            'Market':  cost * calc_market_ratio,
+            'Auction': cost * calc_auction_ratio
+        }
+
 
 def load_input_json(filename):
     with open(filename, "r") as read_file:
@@ -29,5 +57,7 @@ def load_input_json(filename):
 
 if __name__ == '__main__':
     data = load_input_json(INPUT_JSON_FILENAME)
-    print(calc_market_auction("67352", "2007", data))
-    print(calc_market_auction("87964", "2011", data))
+    # print(calc_market_auction("67352", "2007", data))
+    # print(calc_market_auction("87964", "2011", data))
+
+    print(calc_market_auction(sys.argv[1], sys.argv[2], data))
